4721bf2b978c8e5f0ce2887d2d5b4ba94ef7c412
ssrijasdaruk <sirodom.srijasdaruk@accenture.com>
2022-10-19 14:48:58 +1000

    Update stats.py

--- ssg/extensions/stats.py
+++ ssg/extensions/stats.py
@@ -4,20 +4,21 @@ import time
 start_time = None
 total_written = 0
 
-@start_build
+@hooks.register("start_build")
 def start_build():
     global start_time
     start_time = time.time()
 
-@written
+@hooks.register("written")
 def written():
     global total_written
     total_written += 1
 
-@stats
+@hooks.register(stats)
 def stats():
     final_time = start_time - time.time()
     if total_written != 0:
         average = final_time / total_written
+    else 0
     report = "Converted: {} · Time: {:.2f} sec · Ave: {:.4f} sec/file"
     print(report.format(total_written, final_time, average))
